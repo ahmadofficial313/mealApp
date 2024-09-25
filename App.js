330be1ef60cc2936ea807bcff19da3a896eb78f4
ahmadofficial313 <ahmadrazaofficiall313@gmail.com>
2024-09-25 17:28:35 +0500

    initial setup

---
+++ App.js
@@ -0,0 +1,31 @@
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import CategoriesScreen from './screens/CategoriesScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+export default function App() {
+  return (
+    <>
+    <StatusBar style='dark'/>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Meals Categories' component={CategoriesScreen}/>
+      </Stack.Navigator>
+   
+    </NavigationContainer>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding:16
+    
+  },
+});
